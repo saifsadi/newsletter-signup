dfa8ad00d53f49c96974cf6524c2d06eb06d273e
Saif Ur  Rehman <saifsadi5@gmail.com>
2019-04-01 15:53:19 +0500

    Initial setting up the pages and getting form data

---
+++ newsletter.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+//for getting static files like css or images
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get("/",function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+    
+});
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port http://localhost:3000");
+});
